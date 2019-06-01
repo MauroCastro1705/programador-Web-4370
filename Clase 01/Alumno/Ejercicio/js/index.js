ab2a39da801837f18acc6556db33aa397e8e1222
Mauro <maurocastro_17@hotmail.com>
2019-06-01 13:49:08 -0300

    primer ejercicio bien commiteado

---
+++ Clase 01/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,34 @@
+// parter 1
+
+var nombre = prompt()
+console.log('Hola bienvenido: ' + nombre)
+
+// parte 2
+
+var pi = 3.14
+var numeroASumar = prompt()
+numeroASumar = parseFloat(numeroASumar)
+console.log(pi + numeroASumar)
+
+// Parte 3
+
+var numero = prompt('entre un numero a restarle a 10');
+var numeroFijo = 10;
+numero = parseFloat(numero);
+console.log(numeroFijo - numero);
+
+// Parte 4
+
+var numero = prompt('entre un numero para sumarle a 10');
+var numeroFijo = 10;
+numero = parseFloat(numero);
+console.log(numeroFijo + numero);
+
+// parte5
+
+var numeroA = prompt('entre un numero a sumar');
+var numeroB = prompt('entre el segundo numero a sumar');
+
+numeroA = parseFloat(numeroA);
+numeroB = parseFloat(numeroB);
+console.log(numeroA + numeroB);
