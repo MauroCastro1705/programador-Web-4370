9ed886197c3b7e8c65e5957dad58b89d12c10bf9
Mauro <maurocastro_17@hotmail.com>
2019-06-08 16:19:28 -0300

    desafio clase 4

--- Clase 04/Alumno/Desafío/js/index.js
+++ Clase 04/Alumno/Desafío/js/index.js
@@ -30,7 +30,7 @@ estudiante = prompt('Estudiante a Consultar: ').toLocaleLowerCase();
 for (var i = 0; i < studentsList.length; i++) {
   console.log(studentsList[i].dni);
 }
-
+// funcion para buscar por nombre.
 function busqueda(estudiante, listaDeAlumnos) {
   var nombreLower = nombre.toLowerCase();
   for (var i = 0; i < listaDeAlumnos.length; i++) {
@@ -41,4 +41,19 @@ function busqueda(estudiante, listaDeAlumnos) {
   return -1;
 }
 
+//funcion para buscar por nombre y apellido.
+function buscarPorNombreYApellido(nomber, apellido, listaDeAlumnos) {
+  var nombreLower = nombre.toLowerCase();
+  var apellidoLower = apellido.toLowerCase();
+
+  for (var i = 0; i < listaDeAlumnos.length; i++) {
+
+    if (listaDeAlumnos[i].firstName.toLowerCase() === nombreLower
+      && listaDeAlumnos[i].lastName.toLowerCase() === apellidoLower); {
+
+      return i;
+    }
+  }
+
+  //Hacer funcion para buscar por nombre O apellido
 
