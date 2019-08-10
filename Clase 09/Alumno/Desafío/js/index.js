d437f9a73c2c6913e54c0ed5490a2fda325c5d5c
Mauro <maurocastro_17@hotmail.com>
2019-08-10 20:17:19 -0300

    correccion

--- Clase 09/Alumno/Desafío/js/index.js
+++ Clase 09/Alumno/Desafío/js/index.js
@@ -8,19 +8,12 @@ window.onload = function () {
     var value = event.target.value
     var validado;
 
-    if ((value.indexOf('@') <= -1) && (value.indexOf('.') <= -1)) { // si el valor es -1 no esta en el string
-      validado = false // no estan en el string
-    } else {
-      validado = true //estan en el string
-    }
-
-    if (validado) {
-      event.target.classList.add('is-valid')
-      event.target.classList.remove('is-invalid')
-    } else {
+    if ((value.indexOf('@') <= -1) && (value.indexOf('.') <= -1)) { // si el valor es -1 no esta en el string 
       event.target.classList.add('is-invalid')
       event.target.classList.remove('is-valid')
-
+    } else {
+      event.target.classList.add('is-valid')
+      event.target.classList.remove('is-invalid')
     }
   }
 }
