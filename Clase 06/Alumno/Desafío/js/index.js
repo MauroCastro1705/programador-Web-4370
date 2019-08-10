725ac54e80078dbc7642384e7d2c21eaa5cea6b8
Mauro <maurocastro_17@hotmail.com>
2019-08-10 20:02:15 -0300

    correcion del desafio

--- Clase 06/Alumno/Desafío/js/index.js
+++ Clase 06/Alumno/Desafío/js/index.js
@@ -1,21 +1,29 @@
 // js desafio classe 6 astrings y arrays
 
-function includesText(textoA, textoB) {
-  textoA = textoA.toLowerCase();
-  textoB = textoB.toLowerCase();
+var textoA = prompt('Introduzca texto A buscar');
+var textoB = prompt('Introduzca texto DONDE buscar');
 
-  resultado = textoA.indexOf(textoB);
+function includesText(a, b) {
 
-  if ((resultado > -1) && ((typeOf textoA === 'string') && (typeOf textoB === 'string'))  ) {// tiene que ser -1 porqque sino no me tomaria la primer letra del texto ya que empieza a contar en 0
+  var primerTexto;
+  var segundoTexto;
+
+  if (typeof a === 'string') { //comprobacion de si son strings
+    return primerTexto = textoA.toLowerCase()
+  };
+
+  if (typeof b === 'string') {
+    return segundoTexto = textoB.toLowerCase()
+  };
+
+  resultado = primerTexto.indexOf(segundoTexto);
+
+  if (resultado > -1) { // tiene que ser -1 porqque sino no me tomaria la primer letra del texto ya que empieza a contar en 0
     return true
   }
   return false
 
 }
-
-var textoA = prompt('Introduzca texto A buscar');
-var textoB = prompt('Introduzca texto DONDE buscar');
-
 
 if (includesText(textoA, textoB)) {
   console.log('true');
